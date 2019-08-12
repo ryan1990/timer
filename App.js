e90fc3a56894bf81b72fae43db5e76db44bea41c
Ryan Mays <ryanmays1990@gmail.com>
2019-08-12 16:18:26 -0700

    updated App.js with Mitch, other file changes

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Time Practiced</Text>
+      <Text style={styles.titleText}>MM:SS</Text>      
       <Stopwatch />
     </View>
   );
@@ -13,34 +15,54 @@ export default function App() {
 class Stopwatch extends React.Component {
   constructor() {
     super();
+    this.state = {
+      status: "unstarted", // can be "unstarted", "running", or "paused"
+      completeButtonDisabled: true,
+      runningTime: 0
+    };
   }
 
-  // put in constructor as this.state?
-  state = {
-    status: "unstarted", // can be "unstarted", "running", or "paused"
-    completeButtonDisabled: true,
-    runningTime: 0
-  };
+  handleStartPause = () => {
+    // Alternative approach that doesn't use the state callback function, just sets state directly. 
+    // I wouldn't think you would need to check the state within the setState execution. 
+    // This should all evaluate during the click so you should be fine
+    if(this.state.status == "running"){
+      clearInterval(this.timer);
+      this.setState({status: "paused"});
+    } else {
+      const startTime = Date.now() - this.state.runningTime;
+      this.timer = setInterval(() => {
+        this.setState({ runningTime: Date.now() - startTime });
+      }, 1000);
+      this.setState({status: "running"});
+    }
 
-  handleClick = () => {
-    this.setState(state => {
-      if (state.status == "running") {
-        clearInterval(this.timer);
-        this.setState({status: "paused"});
-      } else {
-        const startTime = Date.now() - this.state.runningTime;
-        this.timer = setInterval(() => {
-          this.setState({ runningTime: Date.now() - startTime });
-        }, 1000);
-        this.setState({status: "running"});
-      }
-      return { status: !state.status };
-    });
+    // // OPTION 2: adapted version of your original
+    // // Tried stripping out the embedded set state calls. those seem weird to me.
+    // // They are mixing this.state and using the prevState variable, not sure why they are doing that.
+    // this.setState((prevState) => {
+    //   if (prevState.status == "running") {
+    //     clearInterval(this.timer);
+    //     return{ status: "paused" };
+    //     // this.setState({status: "paused"});
+    //   } else {
+    //     // I'm confused what this sequence does. it seems like you grab the current time multiple times so I'm a bit lost with
+    //     // the setInterval call in there, but if it's not broken, I guess don't fix it. :)
+    //     const startTime = Date.now() - this.state.runningTime;
+    //     this.timer = setInterval(() => {
+    //       this.setState({ runningTime: Date.now() - startTime });
+    //     }, 1000);
+    //     return {status: "running"};
+    //     // this.setState({status: "running"});
+    //   }
+    //   // return { status: !state.status };
+    // });
   };
 
   handleComplete = () => {
     clearInterval(this.timer);
     this.setState({ runningTime: 0, status: false });
+    this.setState({ status: "unstarted"});
   };
 
   componentWillUnmount() {
@@ -58,11 +80,17 @@ class Stopwatch extends React.Component {
     const { status, runningTime } = this.state;
     return (
       <View>
-        <Text>MM:SS</Text>
-        <Text>{this.convertMsToMinutes(runningTime)}</Text>
+        <Text style={styles.countNumbers}>{this.convertMsToMinutes(runningTime)}</Text>
         
-        <Button onPress={this.handleClick} title={status === 'unstarted' ? 'Start New Practice Session' : status === 'running' ? 'Pause Practice Session' : 'Resume Practice Session'} />
-        <Button onPress={this.handleComplete} disabled={this.state.completeButtonDisabled} title="Complete Practice Session" />
+        <Button ViewComponent={LinearGradient} // Don't forget this!
+          linearGradientProps={{
+            colors: ['red', 'pink'],
+            start: { x: 0, y: 0.5 },
+            end: { x: 1, y: 0.5 },
+          }}
+          onPress={this.handleStartPause} title={status === 'unstarted' ? '777Start New Practice Session' : status === 'running' ? 'Pause Practice Session' : 'Resume Practice Session'} />
+        
+        <Button onPress={this.handleComplete} disabled={status === 'unstarted'} title="Complete Practice Session" />
       </View>
     );
   }
@@ -77,4 +105,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  countNumbers: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
